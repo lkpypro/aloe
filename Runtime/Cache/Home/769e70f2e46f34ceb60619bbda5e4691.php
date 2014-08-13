ad6297b8edbdf83fb0d4bdd1cab825e0a7efb9de
lkweb <lk88boy@gmail.com>
2014-08-13 11:19:58 +0800

    代码提交

---
+++ Runtime/Cache/Home/769e70f2e46f34ceb60619bbda5e4691.php
@@ -0,0 +1,329 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>一个好的内容管理框架</title>
+	<link rel="stylesheet" type="text/css" href="http://myjs/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="myblog.css">
+	<script type="text/javascript" src="http://myjs/bootstrap/js/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="http://myjs/bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+	<!-- 头部 -->
+	<!-- 导航条
+================================================== -->
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<div class="navbaar-header">
+					<a class="navbar-brand" href="/index.php?s=/Home/index/index.html">Aloe</a>
+				</div>
+				<button class="navbar-toggle collapsed" data-target=".bs-navbar-collapse" data-toggle="collapse" type="button">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="navbar-collapse bs-navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li>
+							<a href="/index.php?s=/Home/Index/index.html" target="_self">首页</a>
+						</li>
+						<li>
+							<a href="#" target="_self">编程技术</a>
+						</li>
+						<li>
+							<a href="#" target="_self">生活随感</a>
+						</li>
+						<li>
+							<a href="#" target="_self">留言板</a>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="/index.php?s=/Home/User/login.html">登录</a>
+						</li>
+						<li>
+							<a href="/index.php?s=/Home/User/register.html" style="padding-left:0;padding-right:0">注册</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /头部 -->
+
+	<!-- 主体 -->
+
+	<!--导航-->
+	<div style="margin-top:40px;">
+		<header class="jumbotron">
+			<div class="container">
+				<img src="http://myjs/onethink/images/img01.jpg"/>
+			</div>
+		</header>
+	</div>
+	<!--现在位置-->
+	<div class="container">
+		现在位置:
+		<a href="#">首页</a>
+		&gt;
+		<a href="#">网文咨询</a>
+		&gt;
+		<a href="#">文章</a>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-8">
+				<div class="index-article panel panel-default">
+					<div class="entry-header">
+						<div class="title-heavy"></div>
+					</div>
+					<h3>
+						<a href="#" title="这是文章标题">这是文章标题</a>
+						<small>这是副标题</small>
+					</h3>
+					<div class="entry-content">
+						<div class="row">
+							<div class="col-xs-4">
+								<figure class="thumbnail">
+									<a href="#">
+										<img src="http://myjs/onethink/images/1.jpg" alt=""></a>
+								</figure>
+							</div>
+							<div class="col-xs-8">
+								<div class="entry-site">
+									从快播科技5月15日被吊销增值电信业务经营许可证至今，其官方网站依然无法登录。同样紧闭的，还有快播公司的大门和高管们的口风，但与之形成鲜明对比的是，快播生态链上的众多中小视频网站并不甘于沉寂。虽然少了快播这个昔日的大佬，但中小视频网站的江湖却还在。 快播变成了“西瓜” 杨云，一个普通得不能再普通的网友，平时喜欢上网追最新美剧，在他浏览器的视频网站收藏夹中，除了优酷、搜狐视频等之外
+								</div>
+							</div>
+						</div>
+						<div class="entry-meta">
+							<div class="row">
+								<div class="col-md-9">
+									<span class="date">
+								<span class="glyphicon glyphicon-certificate"></span>
+								2014年7月23日
+							</span>
+									<span class="cat">
+								<span class="glyphicon glyphicon-folder-open"></span>
+								热门文章
+							</span>
+								<span class="comment">
+								<a href="#" title="这是文章标题 的评论">
+									<span class="glyphicon glyphicon-comment"></span>
+									评论
+								</a>
+							</span>
+							<span class="views">
+								<span class="glyphicon glyphicon-book"></span>
+								阅读1000条
+							</span>
+						</div>
+							<div class="col-md-3">
+							<span class="entry-more">
+								<a href="#" class="btn btn-primary">阅读全文</a>
+							</span>
+							</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="index-article panel panel-default">
+					<div class="entry-header">
+						<div class="title-heavy"></div>
+					</div>
+					<h3>
+						<a href="#" title="这是文章标题">这是文章标题</a>
+						<small>这是副标题</small>
+					</h3>
+					<div class="entry-content">
+						<div class="row">
+							<div class="col-xs-4">
+								<figure class="thumbnail">
+									<a href="#">
+										<img src="http://myjs/onethink/images/1.jpg" alt=""></a>
+								</figure>
+							</div>
+							<div class="col-xs-8">
+								<div class="entry-site">
+									从快播科技5月15日被吊销增值电信业务经营许可证至今，其官方网站依然无法登录。同样紧闭的，还有快播公司的大门和高管们的口风，但与之形成鲜明对比的是，快播生态链上的众多中小视频网站并不甘于沉寂。虽然少了快播这个昔日的大佬，但中小视频网站的江湖却还在。 快播变成了“西瓜” 杨云，一个普通得不能再普通的网友，平时喜欢上网追最新美剧，在他浏览器的视频网站收藏夹中，除了优酷、搜狐视频等之外
+								</div>
+							</div>
+						</div>
+						<div class="entry-meta">
+							<div class="row">
+								<div class="col-md-9">
+									<span class="date">
+								<span class="glyphicon glyphicon-certificate"></span>
+								2014年7月23日
+							</span>
+									<span class="cat">
+								<span class="glyphicon glyphicon-folder-open"></span>
+								热门文章
+							</span>
+								<span class="comment">
+								<a href="#" title="这是文章标题 的评论">
+									<span class="glyphicon glyphicon-comment"></span>
+									评论
+								</a>
+							</span>
+							<span class="views">
+								<span class="glyphicon glyphicon-book"></span>
+								阅读1000条
+							</span>
+						</div>
+							<div class="col-md-3">
+							<span class="entry-more">
+								<a href="#" class="btn btn-primary">阅读全文</a>
+							</span>
+							</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class="index-article panel panel-default">
+					<div class="entry-header">
+						<div class="title-heavy"></div>
+					</div>
+					<h3>
+						<a href="#" title="这是文章标题">这是文章标题</a>
+						<small>这是副标题</small>
+					</h3>
+					<div class="entry-content">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="entry-site">
+									从快播科技5月15日被吊销增值电信业务经营许可证至今，其官方网站依然无法登录。同样紧闭的，还有快播公司的大门和高管们的口风，但与之形成鲜明对比的是，快播生态链上的众多中小视频网站并不甘于沉寂。虽然少了快播这个昔日的大佬，但中小视频网站的江湖却还在。 快播变成了“西瓜” 杨云，一个普通得不能再普通的网友，平时喜欢上网追最新美剧，在他浏览器的视频网站收藏夹中，除了优酷、搜狐视频等之外
+								</div>
+							</div>
+						</div>
+						<div class="entry-meta">
+							<div class="row">
+								<div class="col-md-9">
+									<span class="date">
+								<span class="glyphicon glyphicon-certificate"></span>
+								2014年7月23日
+							</span>
+									<span class="cat">
+								<span class="glyphicon glyphicon-folder-open"></span>
+								热门文章
+							</span>
+								<span class="comment">
+								<a href="#" title="这是文章标题 的评论">
+									<span class="glyphicon glyphicon-comment"></span>
+									评论
+								</a>
+							</span>
+							<span class="views">
+								<span class="glyphicon glyphicon-book"></span>
+								阅读1000条
+							</span>
+						</div>
+							<div class="col-md-3">
+							<span class="entry-more">
+								<a href="#" class="btn btn-primary">阅读全文</a>
+							</span>
+							</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- 右侧 nav
+================================================== -->
+			<div class="col-xs-4">
+				<div class="blog-widget panel panel-default">
+					<h3 class="widget-title">
+						<span>
+							<span class="glyphicon glyphicon-th-list"></span>
+						热门文章
+						</span>
+					</h3>
+					<ul class="widget-list">
+						<li><a href="#">禁止WordPress后台加载谷歌字体</a></li>
+						<li><a href="#">禁止WordPress后台加载谷歌字体</a></li>
+						<li><a href="#">禁止WordPress后台加载谷歌字体</a></li>
+					</ul>
+				</div>
+				<div class="blog-widget panel panel-default">
+					<h3 class="widget-title">
+						<span>
+							<span class="glyphicon glyphicon-th-list"></span>
+						最新文章
+						</span>
+					</h3>
+					<ul class="widget-list">
+						<li><a href="#">WordPress图片延迟加载</a></li>
+						<li><a href="#">WordPress图片延迟加载</a></li>
+						<li><a href="#">WordPress图片延迟加载</a></li>
+					</ul>
+				</div>
+				<div class="blog-widget panel panel-default">
+					<h3 class="widget-title">
+						<span>
+							<span class="glyphicon glyphicon-th-list"></span>
+						近期留言
+						</span>
+					</h3>
+					<ul class="widget-message">
+						<ul>
+							<li>
+								<a href="#">
+									<img width="32px" height="32px" src="http://myjs/onethink/images/touxiang.png" alt="">
+									<b><span class="comment_author">老虎</span></b> 
+									<p>天天等主题啊鸟哥</p>
+								</a> 
+							</li>
+							<li>
+								<a href="#">
+									<img width="32px" height="32px" src="http://myjs/onethink/images/touxiang.png" alt="">
+									<b><span class="comment_author">老虎</span></b> 
+									<p>天天等主题啊鸟哥</p>
+								</a> 
+							</li>
+							<li>
+								<a href="#">
+									<img width="32px" height="32px" src="http://myjs/onethink/images/touxiang.png" alt="">
+									<b><span class="comment_author">老虎</span></b> 
+									<p>天天等主题啊鸟哥</p>
+								</a> 
+							</li>
+							
+						</ul>
+					</ul>
+				</div>
+			</div>
+			</div>
+	</div>
+
+	<script type="text/javascript"></script>
+	<!-- /主体 -->
+
+	<!-- 底部 -->
+
+	<!-- 底部
+    ================================================== -->
+	<footer class="footer">
+		<div class="site-info">
+			<p>
+				本站由 <strong><a href="http://www.onethink.cn" target="_blank">OneThink</a></strong> 
+				强力驱动
+			</p>
+		</div>
+	</footer>
+
+	<script type="text/javascript"></script>
+	<!-- 用于加载js代码 -->
+	<!-- 页面footer钩子，一般用于加载插件JS文件和JS代码 -->
+	<div class="hidden">
+		<!-- 用于加载统计代码等隐藏元素 -->
+
+	</div>
+
+	<!-- /底部 -->
+</body>
+</html>
